a73d311134a2df70acee4ff0be5948b4b3d5033a
stefano-galati <stefanogalati2002@gmail.com>
2025-09-17 17:37:44 +0200

    Kernel with 4 tasks ruled by 4 semaphores

--- NXP_Startup/main.c
+++ NXP_Startup/main.c
@@ -1,5 +1,6 @@
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 #include <stdio.h>
 #include <string.h>
 #include "uart.h"
@@ -15,6 +16,9 @@ void write_to_UART(void *pvParameters);
 void write_to_CAN_and_transmit(void *pvParameters);
 void read_from_CAN(void *pvParameters);
 
+
+SemaphoreHandle_t xSem1, xSem2, xSem3, xSem4;
+
 //global variables
 char characters[8], characters2[8];
 
@@ -23,12 +27,28 @@ int main(int argc, char **argv){
 	(void) argc;
 	(void) argv;
 
+	//initialize semaphores
+	if((xSem1 = xSemaphoreCreateBinary())==NULL){
+		UART_printf("Semaphore creation failed\n");
+	}
+	if((xSem2 = xSemaphoreCreateBinary())==NULL){
+		UART_printf("Semaphore creation failed\n");
+	}
+	if((xSem3 = xSemaphoreCreateBinary())==NULL){
+		UART_printf("Semaphore creation failed\n");
+	}
+	if((xSem4 = xSemaphoreCreateBinary())==NULL){
+		UART_printf("Semaphore creation failed\n");
+	}
+	//xSem1 is free to be taken
+	xSemaphoreGive(xSem1);
+
 	UART_init();
 	
 	xTaskCreate(read_from_UART, "Task1", 1000, NULL, mainTASK_PRIORITY, NULL);
-	xTaskCreate(write_to_CAN_and_transmit, "Task2", 1000, NULL, mainTASK_PRIORITY-1, NULL);
-	xTaskCreate(read_from_CAN, "Task3", 1000, NULL, mainTASK_PRIORITY-2, NULL);
-	xTaskCreate(write_to_UART, "Task4", 1000, NULL, mainTASK_PRIORITY-3, NULL);
+	xTaskCreate(write_to_CAN_and_transmit, "Task2", 1000, NULL, mainTASK_PRIORITY, NULL);
+	xTaskCreate(read_from_CAN, "Task3", 1000, NULL, mainTASK_PRIORITY, NULL);
+	xTaskCreate(write_to_UART, "Task4", 1000, NULL, mainTASK_PRIORITY, NULL);
 		
 	// Give control to the scheduler
 	vTaskStartScheduler();
@@ -111,69 +131,94 @@ void read_from_UART(void *pvParameters){
 	
 	(void) pvParameters;
 
-	UART_printf("read_from_UART\n");
+	for(;;){
+		xSemaphoreTake(xSem1, portMAX_DELAY);
 
-	UART_printf("Reading 8 chars...\n");
+		UART_printf("read_from_UART\n");
 
-	//read from UART
-	for(int i=0; i<7; i++){
-		characters[i] = UART_getchar();
+		UART_printf("Reading 8 chars...\n");
+
+		//read from UART
+		for(int i=0; i<7; i++){
+			characters[i] = UART_getchar();
+		}
+
+		UART_printf("Bye\n");
+
+		xSemaphoreGive(xSem2);
 	}
-
-	UART_printf("Bye\n");
-	vTaskDelete(NULL);
+	
 }
 
-void write_to_UART(void *pvParameters){
-
-	(void) pvParameters;
-
-	UART_printf("write_to_UART\n");
-
-	char str[50];
-
-	sprintf(str, "%s\n", characters2);
-	UART_printf(str);
-
-	UART_printf("Bye\n");
-	vTaskDelete(NULL);
-}
 
 void write_to_CAN_and_transmit(void *pvParameters){
 
 	(void) pvParameters;
-	UART_printf("write_to_CAN_and_transmit\n");
 
-	//write to CAN
-	int can_id = 0x123;
-	int can_dlc = 8;
-	int is_extended_id = 1;
-	int is_remote_frame = 0;
+	for(;;){
+		xSemaphoreTake(xSem2, portMAX_DELAY);
 
-	//can_data = characters
-	CAN_write(0, can_id, characters, can_dlc, is_extended_id, is_remote_frame);
-	UART_printf("Ok\n");
+		UART_printf("write_to_CAN_and_transmit\n");
 
-	CAN_transmit(0);	//Use CAN1 to transmit
-	UART_printf("Ok\n");
+		//write to CAN
+		int can_id = 0x123;
+		int can_dlc = 8;
+		int is_extended_id = 1;
+		int is_remote_frame = 0;
 
-	UART_printf("Bye\n");
-	vTaskDelete(NULL);
+		//can_data = characters
+		CAN_write(0, can_id, characters, can_dlc, is_extended_id, is_remote_frame);
+
+		CAN_transmit(0);	//Use CAN1 to transmit
+
+		UART_printf("Bye\n");
+		
+		xSemaphoreGive(xSem3);
+	}
+
 }
 
 void read_from_CAN(void *pvParameters){
 
 	(void) pvParameters;
 
-	UART_printf("read_from_CAN\n");
+	for(;;){	
+		xSemaphoreTake(xSem3, portMAX_DELAY);
 
-	if(CAN_has_received(1)){	//Use CAN2 to receive
-		CAN_read_data(1, characters2);
-		CAN_release_receive_buffer(1);
+		UART_printf("read_from_CAN\n");
+
+		if(CAN_has_received(1)){	//Use CAN2 to receive
+			CAN_read_data(1, characters2);
+			CAN_release_receive_buffer(1);
+		}
+
+		UART_printf("Bye\n");
+
+		xSemaphoreGive(xSem4);
 	}
 
-	UART_printf("Bye\n");
-	vTaskDelete(NULL);
+
+}
+
+
+void write_to_UART(void *pvParameters){
+
+	(void) pvParameters;
+
+	for(;;){
+		xSemaphoreTake(xSem4, portMAX_DELAY);
+
+		UART_printf("write_to_UART\n");
+
+		char str[50];
+
+		sprintf(str, "%s\n", characters2);
+		UART_printf(str);
+
+		UART_printf("Bye\n");
+
+		xSemaphoreGive(xSem1);
+	}
 }
 
 // Interrupt handlers
